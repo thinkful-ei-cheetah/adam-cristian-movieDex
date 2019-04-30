2e0a3ff0d3b4365d70d9a0167891200e9f7d7781
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-04-30 15:04:17 -0500

    add boiler plate

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+
+app.use((req, res) => {
+  console.log(process.env.API_TOKEN);
+  res.send('Howdy');
+});
+
+app.listen(8000, () => {
+  console.log('Running on port 8000');
+});
